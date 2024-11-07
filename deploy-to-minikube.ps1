2224648b23259d2f9cb9f3071722478dc73bccc5
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-11-07 22:53:54 +0100

    test

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 #Set Minikube's docker environment
+minikube stop
+
+minikube start
+
 & minikube -p minikube docker-env | Invoke-Expression
 
-
+kubectl apply -f kube/frontend-deployment.yaml       
 kubectl apply -f kube/tweetservice-deployment.yaml
-#Pull images from the local registry
-#docker pull frontend:latest
-#docker pull api-gateway:latest
-#docker pull tweetservice:latest
-#docker pull notifservice:latest
+kubectl apply -f kube/api-gateway-deployment.yaml
 
-#Deploy the images to Minikube
-#kubectl set image deployment/frontend frontend=localhost:4000/frontend:latest
-#kubectl set image deployment/api-gateway api-gateway=localhost:5000/api-gateway:latest
-#kubectl set image deployment/tweetservice tweetservice=localhost:5001/tweetservice:latest
-#kubectl set image deployment/notifservice notifservice=localhost:5002/notifservice:latest
-
-#Restart the pods to apply the changes
-#kubectl rollout restart deployment/frontend
-#kubectl rollout restart deployment/api-gateway
-#kubectl rollout restart deployment/tweetservice
-#kubectl rollout restart deployment/notifservice
+#kubectl expose pod frontend-77658c48c-gk48z --type=NodePort --name=frontend-service
