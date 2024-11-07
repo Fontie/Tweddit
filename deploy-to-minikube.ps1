4dc6db8e8f8352b6508e9fc64654227f00ab6731
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-11-07 23:17:50 +0100

    eafknj

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -9,6 +9,7 @@ minikube start
 
 kubectl apply -f ./kube/frontend-deployment.yaml       
 kubectl apply -f ./kube/tweetservice-deployment.yaml
-kubectl apply -f ./kube/api-gateway-deployment.yaml
+kubectl apply -f ./kube/notifservice-deployment.yaml
+kubectl apply -f ./kube/api-gateway.yaml
 
 #kubectl expose pod frontend-77658c48c-gk48z --type=NodePort --name=frontend-service
