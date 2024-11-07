544b197fbc3ebd107bfd98f04396995de1f0546e
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-11-07 22:39:08 +0100

    git test 24

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -3,20 +3,22 @@
 #Set Minikube's docker environment
 & minikube -p minikube docker-env | Invoke-Expression
 
+
+kubectl apply -f kube/tweetservice-deployment.yaml
 #Pull images from the local registry
-docker pull localhost:4000/frontend:latest
-docker pull localhost:5000/api-gateway:latest
-docker pull localhost:5001/tweetservice:latest
-docker pull localhost:5002/notifservice:latest
+#docker pull frontend:latest
+#docker pull api-gateway:latest
+#docker pull tweetservice:latest
+#docker pull notifservice:latest
 
 #Deploy the images to Minikube
-kubectl set image deployment/frontend frontend=localhost:4000/frontend:latest
-kubectl set image deployment/api-gateway api-gateway=localhost:5000/api-gateway:latest
-kubectl set image deployment/tweetservice tweetservice=localhost:5001/tweetservice:latest
-kubectl set image deployment/notifservice notifservice=localhost:5002/notifservice:latest
+#kubectl set image deployment/frontend frontend=localhost:4000/frontend:latest
+#kubectl set image deployment/api-gateway api-gateway=localhost:5000/api-gateway:latest
+#kubectl set image deployment/tweetservice tweetservice=localhost:5001/tweetservice:latest
+#kubectl set image deployment/notifservice notifservice=localhost:5002/notifservice:latest
 
 #Restart the pods to apply the changes
-kubectl rollout restart deployment/frontend
-kubectl rollout restart deployment/api-gateway
-kubectl rollout restart deployment/tweetservice
-kubectl rollout restart deployment/notifservice
+#kubectl rollout restart deployment/frontend
+#kubectl rollout restart deployment/api-gateway
+#kubectl rollout restart deployment/tweetservice
+#kubectl rollout restart deployment/notifservice
