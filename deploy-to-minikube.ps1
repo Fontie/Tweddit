93a60345f951af3312b1d16a22cfe21e33e4e8b7
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-11-08 00:32:30 +0100

    possible finish

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -28,12 +28,13 @@ kubectl apply -f ./kube/notifservice-deployment.yaml
 $frontPodName = $(kubectl get pods -l app=frontend -o jsonpath='{.items[0].metadata.name}')
 
 kubectl expose pod $frontPodName --type=NodePort --name=frontend-service
+minikube service frontend-service
 
-$minikubeIp = minikube ip
-$nodePort = kubectl get svc frontend-service -o jsonpath='{.spec.ports[0].nodePort}'
+#$minikubeIp = minikube ip
+#$nodePort = kubectl get svc frontend-service -o jsonpath='{.spec.ports[0].nodePort}'
 
-$serviceUrl = "http://$($minikubeIp):$($nodePort)"
-Write-Output "CHECK THIS LINK!!!!: $serviceUrl"
+#$serviceUrl = "http://$($minikubeIp):$($nodePort)"
+#Write-Output "CHECK THIS LINK!!!!: $serviceUrl"
 
 
 #kubectl expose pod frontend-77658c48c-gk48z --type=NodePort --name=frontend-service
