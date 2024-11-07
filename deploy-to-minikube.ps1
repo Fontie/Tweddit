a7ac1704801bfedda12da28a9334cef249ca5885
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-11-07 23:08:32 +0100

    test 30

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -7,8 +7,8 @@ minikube start
 
 & minikube -p minikube docker-env | Invoke-Expression
 
-kubectl apply -f kube/frontend-deployment.yaml       
-kubectl apply -f kube/tweetservice-deployment.yaml
-kubectl apply -f kube/api-gateway-deployment.yaml
+kubectl apply -f ./kube/frontend-deployment.yaml       
+kubectl apply -f ./kube/tweetservice-deployment.yaml
+kubectl apply -f ./kube/api-gateway-deployment.yaml
 
 #kubectl expose pod frontend-77658c48c-gk48z --type=NodePort --name=frontend-service
