c2aab9c2321612e1f3a43deba063e4aa4b0cd0e5
Michael Bednarek <55832436+Fontie@users.noreply.github.com>
2024-12-10 23:36:17 +0100

    need new images

--- deploy-to-minikube.ps1
+++ deploy-to-minikube.ps1
@@ -46,3 +46,62 @@ Write-Output "minikube service frontend-service"
 
 
 #kubectl expose pod frontend-77658c48c-gk48z --type=NodePort --name=frontend-service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ SNYK-DOTNET-MICROSOFTASPNETCOREWEBSOCKETS-72891:
+    - '*':
+        reason: This vulnerability does not affect my project, as we are not using the affected version.
+        expires: 2025-01-09T12:54:24.659Z
+        created: 2024-12-10T12:54:24.662Z
+  SNYK-DOTNET-MICROSOFTASPNETCOREWEBSOCKETS-72898:
+    - '*':
+        reason: This vulnerability does not affect my project, as we are not using the affected version.
+        expires: 2025-01-09T12:54:24.659Z
+        created: 2024-12-10T12:54:24.662Z
+  SNYK-DOTNET-NEWTONSOFTJSON-2774678:
+    - '*':
+        reason: This vulnerability has been fixed in later versions. We will upgrade as soon as feasible.
+        expires: 2025-01-09T12:54:24.659Z
+        created: 2024-12-10T12:54:24.662Z
+  SNYK-DOTNET-SYSTEMNETWEBSOCKETSWEBSOCKETPROTOCOL-72894:
+    - '*':
+        reason: This vulnerability does not affect my project, as we are not using the affected version.
+        expires: 2025-01-09T12:54:24.659Z
+        created: 2024-12-10T12:54:24.662Z
+  SNYK-DOTNET-SYSTEMTEXTENCODINGSWEB-1253267:
+    - '*':
+        reason: This vulnerability has been fixed in later versions. We will upgrade as soon as feasible.
+        expires: 2025-01-09T12:54:24.659Z
+        created: 2024-12-10T12:54:24.662Z
